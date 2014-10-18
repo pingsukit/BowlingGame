fa77241bc5ee55e37142b694a63d58aedfa005fd
pingsukit <pingsukit@pingsukit.local>
2014-10-18 18:22:06 +0700

    finished

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,11 +1,52 @@
 class Game
 	def initialize
-     @score = 0
+     
+     @rolls = []
     end
+
 	def roll(pins)
-		@score = score + pins
+		@rolls << pins
 	end
+
 	def score()
-		@score 
+		score=0
+		frame=0
+		index=0
+		while frame<10
+			if frame<9
+				if strike(index)
+					score+=10+@rolls[index+1]+@rolls[index+2]
+					index+=1
+				elsif spare(index)
+					score+=10+@rolls[index+2]
+					index+=2
+				else 
+					score+=@rolls[index]+@rolls[index+1]
+					index+=2
+				end
+
+			elsif frame==9 #last frame
+				if strike(index)
+					score+=10+@rolls[index+1]+@rolls[index+2]
+				elsif spare(index)
+					score+=10+@rolls[index+2]
+				elsif @rolls[index]+@rolls[index+1]<10
+					score+=@rolls[index]+@rolls[index+1]
+				end
+			end
+			frame+=1
+		end
+		 # @rolls.each do |roll|
+   #      	 @score += roll
+	  #    end
+	  score
 	end
+
+	def spare(index)
+    	@rolls[index] + @rolls[index + 1] == 10
+   end
+
+   def strike(index)
+    	@rolls[index]== 10
+   end
 end
--- spec/lib/Game_spec.rb
+++ spec/lib/Game_spec.rb
@@ -6,24 +6,173 @@ describe Game do
 	g = Game.new
 	end
 
-	describe "#roll" do
-		it "roll the ball" do
+	describe "prove score and roll method" do
+		it "frame 10 strike" do
 			g = Game.new
-			#g.roll(1)
-			20.times do
-    			g.roll(0)
-   			end
-			expect(g.score).to eq(0)
-			g1 = Game.new
-			#g.roll(1)
-			20.times do
-    			g1.roll(1)
-   			end
-			expect(g1.score).to eq(20)
+			  #frame1
+		      g.roll(5)
+		      g.roll(5) #spare
+		      #frame2
+		      g.roll(3)
+		      g.roll(4)
+		      #frame3
+		      g.roll(10) #strike
+		      #frame4
+		      g.roll(5)
+		      g.roll(3)
+		      #frame5
+		      g.roll(7)
+		      g.roll(3) #spare
+		      #frame6
+		      g.roll(10) #strike
+		      #frame7
+		      g.roll(8)
+		      g.roll(2) #spare
+		      #frame8
+		      g.roll(2)
+		      g.roll(2)
+		      #frame9
+		      g.roll(4)
+		      g.roll(6) #spare
+		      #frame10
+		      g.roll(10)#strike
+		      g.roll(2)
+		      g.roll(5) #row3
+		      expect(g.score).to eq(139)
+					
 		end
+
+		it "no spare or strike at frame 10" do
+			g = Game.new
+			  #frame1
+		      g.roll(5)
+		      g.roll(1) 
+		      #frame2
+		      g.roll(3)
+		      g.roll(4)
+		      #frame3
+		      g.roll(10) #strike
+		      #frame4
+		      g.roll(5)
+		      g.roll(3)
+		      #frame5
+		      g.roll(7)
+		      g.roll(3) #spare
+		      #frame6
+		      g.roll(10) #strike
+		      #frame7
+		      g.roll(8)
+		      g.roll(2) #spare
+		      #frame8
+		      g.roll(2)
+		      g.roll(2)
+		      #frame9
+		      g.roll(4)
+		      g.roll(6) #spare
+		      #frame10
+		      g.roll(2)
+		      g.roll(2)
+		       #no row3
+		      expect(g.score).to eq(111)
+					
+		end
+
+		it 'can roll a spare at last frame' do
+			g2 = Game.new
+		    #frame1
+		    g2.roll(5)
+		    g2.roll(5) #spare
+		    #frame2
+		    g2.roll(3)
+		    g2.roll(4)
+		    #frame3
+		    g2.roll(10) #strike
+		    #frame4
+		    g2.roll(5)
+		    g2.roll(3)
+		    #frame5
+		    g2.roll(7)
+		    g2.roll(3) #spare
+		    #frame6
+		    g2.roll(10) #strike
+		    #frame7
+		    g2.roll(8)
+		    g2.roll(2) #spare
+		    #frame8
+		    g2.roll(2)
+		    g2.roll(2)
+		    #frame9
+		    g2.roll(4)
+		    g2.roll(6) #spare
+		    #frame10
+		    g2.roll(8)
+		    g2.roll(2) #spare
+		    g2.roll(5) #row3
+		    expect(g2.score).to eq(135)
+	    end
+
+		it 'can roll a strike at last frame' do
+			g3 = Game.new
+		    #frame1
+		    g3.roll(4)
+		    g3.roll(1)
+		    #frame2
+		    g3.roll(3)
+		    g3.roll(2)
+		    #frame3
+		    g3.roll(4)
+		    g3.roll(6) #spare
+		    #frame4
+		    g3.roll(10) #strike
+		    #frame5
+		    g3.roll(5)
+		    g3.roll(5) #spare
+		    #frame6
+		    g3.roll(2)
+		    g3.roll(8) #strike
+		    #frame7
+		    g3.roll(2)
+		    g3.roll(1)
+		    #frame8
+		    g3.roll(2)
+		    g3.roll(8) #strike
+		    #frame9
+		    g3.roll(10)	#strike
+		    #frame10
+		    g3.roll(10) #strike
+		    g3.roll(6) 
+		    g3.roll(2) #row3
+		    expect(g3.score).to eq(141)
+	    end
+
+	    it "perfect game(all strike)" do
+			g = Game.new
+			  #frame1
+		      g.roll(10) #strike
+		      #frame2
+		      g.roll(10) #strike
+		      #frame3
+		      g.roll(10) #strike
+		      #frame4
+		      g.roll(10) #strike
+		      #frame5
+		      g.roll(10) #strike
+		      #frame6
+		      g.roll(10) #strike
+		      #frame7
+		      g.roll(10) #strike
+		      #frame8
+		      g.roll(10) #strike
+		      #frame9
+		      g.roll(10) #strike
+		      #frame10
+		      g.roll(10) #strike
+		      g.roll(10) #strike
+		      g.roll(10) #strike
+		      expect(g.score).to eq(300)
+					
+		end
+
 	end
-	describe "#score" do
-		it "final score"do
-	end
-	end
+	
 end
