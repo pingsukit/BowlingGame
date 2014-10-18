98bd2684c07f380d93d22e2c82adf6443a76e2f9
pingsukit <pingsukit@pingsukit.local>
2014-10-18 15:41:38 +0700

    method caption

---
+++ spec/lib/Game_spec.rb
@@ -0,0 +1,19 @@
+require "Game"
+require "spec_helper"
+
+describe Game do 
+	it "to create a new game"do
+	g = Game.new
+	end
+
+	describe "#roll" do
+		it "roll the ball" do
+			
+			
+		end
+	end
+	describe "#score" do
+		it "final score"do
+	end
+	end
+end
