2faab9a1a60bfea1b3763a52695fdc3a5f81e109
pingsukit <pingsukit@pingsukit.local>
2014-10-18 16:00:21 +0700

    basic roll ball

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,8 +1,11 @@
 class Game
+	def initialize
+     @score = 0
+    end
 	def roll(pins)
-		
+		@score = score + pins
 	end
 	def score()
-		
+		@score 
 	end
 end
--- spec/lib/Game_spec.rb
+++ spec/lib/Game_spec.rb
@@ -8,8 +8,18 @@ describe Game do
 
 	describe "#roll" do
 		it "roll the ball" do
-			
-			
+			g = Game.new
+			#g.roll(1)
+			20.times do
+    			g.roll(0)
+   			end
+			expect(g.score).to eq(0)
+			g1 = Game.new
+			#g.roll(1)
+			20.times do
+    			g1.roll(1)
+   			end
+			expect(g1.score).to eq(20)
 		end
 	end
 	describe "#score" do
